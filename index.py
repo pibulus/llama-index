1a0d19eed2f8355abab7c2eb061a1a5f41696cdd
pibulus <pibulus@gmail.com>
2023-06-26 00:59:03 +1000

    updated

--- index.py
+++ index.py
@@ -1,36 +1,41 @@
-import os, streamlit as st
+import os
+import streamlit as st
+import openai
+from airtable import Airtable
+from llama_index import (
+    GPTVectorStoreIndex,
+    LLMPredictor,
+    PromptHelper,
+    ServiceContext
+)
+from langchain import OpenAI
 
-# Uncomment to specify your OpenAI API key here (local testing only, not in production!), or add corresponding environment variable (recommended)
-# os.environ['OPENAI_API_KEY']= ""
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+Airtable_TOKEN = os.getenv('AIRTABLE_TOKEN')
 
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
-from langchain.llms.openai import OpenAI
+openai.api_key = OPENAI_API_KEY
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
-# Define a simple Streamlit app
 st.title("Ask Llama")
-query = st.text_input("What would you like to ask? (source: data/paul_graham_essay.txt)", "")
+query = st.text_input("What would you like to ask? (source: Airtable)", "")
 
-# If the 'Submit' button is clicked
 if st.button("Submit"):
     if not query.strip():
         st.error(f"Please provide the search query.")
     else:
         try:
-            # This example uses text-davinci-003 by default; feel free to change if desired
-            llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+            airtable = Airtable('appUkRhauFCWrTrBd', 'tblP6LQxyOo7JBiJC', api_key=Airtable_TOKEN)
+            documents = airtable.get_all()
 
-            # Configure prompt parameters and initialise helper
+            # define prompt helper
             max_input_size = 4096
             num_output = 256
             max_chunk_overlap = 20
 
-            prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+            llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.1, model_name="text-davinci-003", max_tokens=num_output))
 
-            # Load documents from the 'data' directory
-            documents = SimpleDirectoryReader('data').load_data()
-            service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-            index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-            
+            index = GPTVectorStoreIndex.from_documents(documents)
+
             response = index.query(query)
             st.success(response)
         except Exception as e:
